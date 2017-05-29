3523a9ba3993cc1bf82de2be363bff82058de7b3
“Joshua <JoshuaRinard@Joshuas-iMac.local>
2017-05-28 19:55:07 -0700

    basic stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,16 +2,15 @@ class StatsController < ApplicationController
 
   def index
     @user = current_user
-  end
-
-  def show
-  end
-
-  def new
-  end
-
-
-  def create
+    @units = Unit.all
+    @units7 = User.where("date(created_at) > ?", 6.days.ago).count
+    @units30 = User.where("date(created_at) > ?", 30.days.ago).count
+    @units365 = User.where("date(created_at) > ?", 365.days.ago).count
+    @parts = Part.all
+    @parts7 = User.where("date(created_at) > ?", 6.days.ago).count
+    @parts30 = Part.where("date(created_at) > ?", 30.days.ago).count
+    @parts365 = Part.where("date(created_at) > ?", 365.days.ago).count
+    @users = User.all
   end
 
 end
