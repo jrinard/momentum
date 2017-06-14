2cd9f1cfc963912cb67da874e8d2a467ec48d5ce
“Joshua <JoshuaRinard@Joshuas-iMac.local>
2017-06-14 11:29:19 -0700

    Basic Readme, added to seeds

--- README.md
+++ README.md
@@ -1,24 +1,53 @@
-# README
+### Momentum App
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Momentum is a software that allows non profit organizations or social movements to organize their internal structure.
 
-Things you may want to cover:
+* Ruby version - 2.2.2
 
-* Ruby version
+### Database Setup
 
-* System dependencies
+ * `postgres` in Terminal
+ * `rails db:setup`
+ * `rails db:seed`
 
-* Configuration
+### Run Ap
 
-* Database creation
+* `bundle install`
+* `rails s`
+* Visit localhost:3000 in browser
 
-* Database initialization
 
-* How to run the test suite
+### USER Stories
 
-* Services (job queues, cache servers, search engines, etc.)
+* As a admin you are able to create families and people that belong to those families
+* As a admin you are able to create departments and assign people to those departments
+* As a admin you are able to create events and check people into those events
+* As a admin you are able to view statistics total users, families, people, and track attendance and movement growth over time
 
-* Deployment instructions
+### USER Stories - still to do
+* Build out UI for dashboard and stats
+* As a admin you are able to create event needs
+* As a admin you are able to sign people to serve in those events to meet needs
+* As a user you are able to create an account. and view your dashboard
+* As a user you are able to see news, upcoming events, and serving opportunities on your dashboard
+* As a user you are able to sign up to serve in an event or department
+* As a admin you are able to approve and assign users serving in an event or department
+* As a user you will be able to give money or pay for group activities
+* As a user you will be able to become a leader and establish a group
+* As a leader you will be able to add people to your group and group details
+* As a leader you will be able to message your group members
+* As a user you will be able to email other people
+* As a user you will be able to report a bug or development suggestion
 
-* ...
+
+### Support and Contact Details
+
+* Github for Joshua Rinard: https://github.com/jrinard
+
+* Email: jrinard@hotmail.com
+
+### License
+
+MIT
+
+Copyright (c) 2017 Joshua Rinard
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
-# //Not the same as current_user
+user = User.create(id: 100, email: "j@email.com", password: 'password', password_confirmation: 'password', created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05")
 
-user = User.create(id: 100, email: "jrinard@hotmail.com", password: 'password', password_confirmation: 'password', created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05")
-
-unit = Unit.create(id: 100, familyname: "Rinard", street: nil, city: nil, state: nil, zip: nil, country: nil, phonePrimary: nil, phoneSecondary: nil, emailPrimary: nil, emailSecondary: nil, notes: nil, accountOwner: "jrinard@hotmail.com", created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05", user_id: 100)
-
-part = Part.create(id: 100, title: "Mr.", firstname: 'Joshua', lastname: 'Rinard', allergies: nil, parent: nil, birthdate: nil, backCheck: nil, notes: nil, member: nil, leader: nil, unit_id: 100, created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05")
-
-department = Department.create(id: 100, name: "Worship", description: "details", leaderName: "Joshua", part_id: 100, created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05")
+unit = Unit.create(id: 100, familyname: "Rinard", street: nil, city: nil, state: nil, zip: nil, country: nil, phonePrimary: nil, phoneSecondary: nil, emailPrimary: nil, emailSecondary: nil, notes: nil, accountOwner: "j@email.com", created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05", user_id: 100)
+unit2 = Unit.create(id: 101, familyname: "Jobs", street: nil, city: nil, state: nil, zip: nil, country: nil, phonePrimary: nil, phoneSecondary: nil, emailPrimary: nil, emailSecondary: nil, notes: nil, accountOwner: "j@email.com", created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05", user_id: 100)
 
 
-event = Event.create(id: 1, name: "Sunday Seed", description: "Powerful Vestibulum mi turpis, rhoncus et molestie sed, tristique ac ante. Ut id porta est. Nulla facilisi. In dapibus id lectus sit amet viverra. Etiam enim odio, dapibus in cursus non, interdum in nunc. Nunc vel orci turpis. Donec aliquet semper velit, sit amet placerat dolor semper nec.", date: "2017-01-02", starttime: "2000-01-01 19:30:00", endtime: nil, locationname: "Main Campus", street: "1 Infinit loop", city: "Vancouver ", state: "Washington", zip: 98683, country: "United States", recurring: false, attendance: nil, volunteers: nil, created_at: "2017-06-08 23:11:36", updated_at: "2017-06-09 20:42:38")
+part = Part.create(id: 100, title: "Mr.", firstname: 'Joshua', lastname: 'Rinard', allergies: nil, parent: nil, birthdate: "2000-01-01", backcheck: nil, notes: nil, member: nil, leader: nil, unit_id: 100, created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05")
+part2 = Part.create(id: 101, title: "Mr.", firstname: 'Steve', lastname: 'Jobs', allergies: nil, parent: nil, birthdate: "2000-01-01", backcheck: nil, notes: nil, member: nil, leader: nil, unit_id: 101, created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05")
+
+department = Department.create(id: 100, name: "Marketing", description: "details", leaderName: "Joshua", part_id: 100, created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05")
+department2 = Department.create(id: 102, name: "R&D", description: "details", leaderName: "Joshua", part_id: 100, created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05")
+
+event = Event.create(id: 1, name: "Staff Lunch", description: "Fancy Vestibulum mi turpis, rhoncus et molestie sed, tristique ac ante. Ut id porta est. Nulla facilisi. In dapibus id lectus sit amet viverra. Etiam enim odio, dapibus in cursus non, interdum in nunc. Nunc vel orci turpis. Donec aliquet semper velit, sit amet placerat dolor semper nec.", date: "2017-06-25", starttime: "2000-01-01 12:30:00", endtime: nil, locationname: "Main Campus", street: "1 Infinite loop", city: "Vancouver ", state: "Washington", zip: 98683, country: "United States", recurring: false, attendance: nil, volunteers: nil, created_at: "2017-06-08 23:11:36", updated_at: "2017-06-09 20:42:38")
+
+event2 = Event.create(id: 2, name: "Friday Vision", description: "Vision Vestibulum mi turpis, rhoncus et molestie sed, tristique ac ante. Ut id porta est. Nulla facilisi. In dapibus id lectus sit amet viverra. Etiam enim odio, dapibus in cursus non, interdum in nunc. Nunc vel orci turpis. Donec aliquet semper velit, sit amet placerat dolor semper nec.", date: "2017-06-30", starttime: "2000-01-01 8:30:00", endtime: nil, locationname: "Main Office", street: "1 Infinite loop", city: "Vancouver ", state: "Washington", zip: 98683, country: "United States", recurring: false, attendance: nil, volunteers: nil, created_at: "2017-06-08 23:11:36", updated_at: "2017-06-09 20:42:38")
