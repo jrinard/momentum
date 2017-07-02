8c22f62fe922a1f159189a4849ffd2e1132dfa3b
“Joshua <JoshuaRinard@Joshuas-iMac.local>
2017-07-01 22:59:14 -0700

    Dashboard calendar and basic formating

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@ class HomeController < ApplicationController
   def index
     @user = current_user
     @unit = Unit.order(updated_at: :desc)
+    @revents = Revent.all
+    @calendar_events = @revents.flat_map{ |e| e.calendar_events(params.fetch(:start_date, Time.zone.now).to_date) }   # running calendar_events method and passing start date tells our recurring events where we want to grab.. thinking about exceptions
+
   end
 
   def show
