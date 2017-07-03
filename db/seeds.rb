a6a3cb77eca3cc4d2f090aaf942ecaf9879eca95
“Joshua <JoshuaRinard@Joshuas-iMac.local>
2017-07-03 00:08:09 -0700

    fix seed for accountowner change

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 user = User.create(id: 100, email: "j@email.com", password: 'password', password_confirmation: 'password', created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05")
 
-unit = Unit.create(id: 100, familyname: "Rinard", street: nil, city: nil, state: nil, zip: nil, country: nil, phonePrimary: nil, phoneSecondary: nil, emailPrimary: nil, emailSecondary: nil, notes: nil, accountOwner: "j@email.com", created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05", user_id: 100)
-unit2 = Unit.create(id: 101, familyname: "Jobs", street: nil, city: nil, state: nil, zip: nil, country: nil, phonePrimary: nil, phoneSecondary: nil, emailPrimary: nil, emailSecondary: nil, notes: nil, accountOwner: "j@email.com", created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05", user_id: 100)
+unit = Unit.create(id: 100, familyname: "Rinard", street: nil, city: nil, state: nil, zip: nil, country: nil, phonePrimary: nil, phoneSecondary: nil, emailPrimary: nil, emailSecondary: nil, notes: nil, accountowner: "j@email.com", created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05", user_id: 100)
+unit2 = Unit.create(id: 101, familyname: "Jobs", street: nil, city: nil, state: nil, zip: nil, country: nil, phonePrimary: nil, phoneSecondary: nil, emailPrimary: nil, emailSecondary: nil, notes: nil, accountowner: "j@email.com", created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05", user_id: 100)
 
 
 part = Part.create(id: 100, title: "Mr.", firstname: 'Joshua', lastname: 'Rinard', allergies: nil, parent: nil, birthdate: "2000-01-01", backcheck: nil, notes: nil, member: nil, leader: nil, unit_id: 100, created_at: "2017-05-24 22:24:05", updated_at: "2017-05-24 22:24:05")
